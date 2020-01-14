33b535b3491cb054fb7e8973bfe1cb205ef89de0
Marco <1997mrc@gmail.com>
2020-01-14 22:09:18 +0100

    add .htaccess

--- js/main.js
+++ js/main.js
@@ -10,14 +10,18 @@ var attivaConsoleLog = true;
 // il parametro passato è una stringa che corrisponde al path
 // dello script che si vuole caricare
 function loadScript(link) {
-    $.getScript(link, function() {
+    $.getScript(link, function () {
         if (attivaConsoleLog) {
             console.log("Script '" + link + "' caricato con successo");
         }
-    }).fail(function() {
+    }).fail(function () {
         if (attivaConsoleLog) {
-            console.log("Errore durante il caricamento dello script" + link);            
+            console.log("Errore durante il caricamento dello script" + link);
         }
     });
-} 
+}
 
+$(document).ready(() => {
+    console.log("is' alive");
+    loadScript("./plugins/popper/popper.min.js");
+});
