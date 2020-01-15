35b4d0a9ac70a5ec7cf1c009052597b407564d44
Accelerator7460 <michele7460@gmail.com>
2020-01-15 17:28:37 +0100

    aggiunto plugin animate.js - div di popup success o failure

--- js/main.js
+++ js/main.js
@@ -10,18 +10,66 @@ var attivaConsoleLog = true;
 // il parametro passato è una stringa che corrisponde al path
 // dello script che si vuole caricare
 function loadScript(link) {
-    $.getScript(link, function () {
+    $.getScript(link, function() {
         if (attivaConsoleLog) {
             console.log("Script '" + link + "' caricato con successo");
         }
-    }).fail(function () {
+    }).fail(function() {
         if (attivaConsoleLog) {
-            console.log("Errore durante il caricamento dello script" + link);
+            console.log("Errore durante il caricamento dello script" + link);            
         }
     });
 }
 
-$(document).ready(() => {
-    console.log("is' alive");
-    loadScript("./plugins/popper/popper.min.js");
-});
+
+// Ritorna il path assoluto di una qualsia risorsa
+// Parametri accettati
+// Tipo risorsa: in base al tipo della risorsa passata crea il path assoluto
+// Utente: serve se si deve caricare / scaricare da una sottocartella di Files (es /Files/10/...)
+// se l'utente non viene specificato, allora si farà riferimento alla cartella /Files/public/...
+function downloadRisorsa(idUtente, tipoRisorsa, idRisorsa) {
+    var objParam = {};
+    objParam.action = "downloadRisorsa";
+    objParam.idUtente = idUtente;
+    objParam.tipoRisorsa = tipoRisorsa;
+    objParam.idRisorsa = idRisorsa;
+
+    // funzione lato controller per copia file su /Temp/idUtente/tipoRisorsa
+    $.ajax({
+        url: "/Controller/NomeController.php",
+        cache: false,
+        data: objParam
+    }).done(function (data) {
+        var obj = JSON.parse(data);
+        if (obj.status) {
+            var link = document.createElement("a");
+            document.body.appendChild(link);
+            link.download = obj.data.nomeFile;
+            link.href = obj.data.pathFile;
+            link.click();
+            $(link).remove();
+            popupSuccess();
+        } else {
+            popupFailure(obj.msg);
+        }
+    });
+
+}
+
+
+// Mostra una bootbox di operazione avvenuta correttamente
+// Può essere passato anche un messaggio da mostrare
+function popupSuccess(msg) {
+    bootbox.alert({
+        message: "Operazione avvenuta con successo. " + !msg ? ' ' : msg
+    });
+}
+
+
+// Mostra una bootbox di errore durante un'operazione
+// Può essere passato anche un messaggio da mostrare
+function popupFailure(msg) {
+    bootbox.alert({
+        message: "Errore durante l'operazione.<br />Contattare un amministratore ed indicare il seguente codice: " + !msg ? ' ' : msg
+    });
+}
